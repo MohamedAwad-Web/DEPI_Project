62c7d934aac3008cf81dc2f14a2d72dd52ae1ba1
Mohamed_Awad_Elgohary <mohamedawad0083@gmail.com>
2025-11-20 23:08:03 +0200

    0.0

--- Bookify Hotel Reservation System/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bookify_Hotel_Reservation_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
